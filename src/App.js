f74f25e738f786a2e38900a049831b907a4aa041
metin137 <mtnklc137@gmail.com>
2022-03-03 22:27:11 +0300

    architacture refactored

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+
 // Screens
 import Landing from "./screens/Landing.jsx";
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "./style/flexboxgrid.min.css";
-import './style/index.css';
-import './index.css';
-import './styles/main.less';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "src/styles/flexboxgrid.min.css";
+import 'src/styles/index.css';
+import 'src/styles/main.less';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -17,7 +16,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
